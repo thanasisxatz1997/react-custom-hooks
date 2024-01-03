89a701eefefe8bfe31cc5ee46aa8a862a70953c8
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-01-03 11:32:57 +0200

    Create useKey.js

---
+++ useKey.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+//Hook that takes in a string for the key to be pressed and an action that will happend when the key is pressed
+
+export function useKey(key, action) {
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code.toLowerCase() === key.toLowerCase()) {
+          action();
+        }
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [action, key]
+  );
+}
