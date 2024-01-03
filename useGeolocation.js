5a86e26e57934aaef7d9dcdf82e9c9c7c5b82d82
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-01-03 12:37:44 +0200

    Create useGeolocation.js

---
+++ useGeolocation.js
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+
+//Hook that fetches current geolocation, returns a getPosition function that can be called to refresh, and a position that includes current latitude and longitude.
+
+export function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+    
+  return { isLoading, position, error, getPosition};
+}
